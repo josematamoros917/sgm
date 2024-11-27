1c0c57645133dad8dc342b611d51c1ac3735edda
josematamoros917 <josematamoros917@gmail.com>
2024-11-26 23:00:14 -0300

    configuracion del wsgi.py

--- configs/wsgi.py
+++ configs/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/skullgaragemotorcycle/sgm'  # Ruta al directorio raíz de tu proyecto en PythonAnywhere
+if path not in sys.path:
+    sys.path.append(path)
+
+# Aquí indicamos que las configuraciones están en el directorio 'configs' y el archivo se llama 'settings.py'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'configs.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings')
-
 application = get_wsgi_application()
+
+
