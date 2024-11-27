76ba626cc5beb20a6b22bc03028e7a5736d743b3
josematamoros917 <josematamoros917@gmail.com>
2024-11-26 22:38:48 -0300

    otro commit de settings, ahora incluyendo DEBUG=False

--- configs/settings.py
+++ configs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k969i#oy9f$h4hwbbb%6y3qeij7gi1=_%+=!ri7rc5oky@dvt!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['skullgaragemotorcycle.pythonanywhere.com']
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
